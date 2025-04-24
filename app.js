87c678704ebe1d7be98c4063f87eefd0b5810dcc
emesefedev <emesefedev@gmail.com>
2025-04-24 16:10:38 +0200

    create index and new Routers

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => res.send("Hello, Emesefe!"))
+const newRouter = require("./routes/newRouter")
+const indexRouter = require("./routes/indexRouter") 
+
+app.use("/new", newRouter)
+app.use("/", indexRouter)
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`My first Express app - listening on port ${port}!`);
+  console.log(`My first Express app - listening on port ${port}!`)
 });

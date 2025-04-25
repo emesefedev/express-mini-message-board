c80e0be293f7b27b2faf7480156f7d78038f8694
emesefedev <emesefedev@gmail.com>
2025-04-25 12:02:20 +0200

    style messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,12 @@
 const { Router } = require("express");
+const { getMessages } = require('../controllers/messagesController')
+const { formatDate } = require("../utilities/dateFormatter")
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("Hello, Emesefe!"))
+indexRouter.get("/", async (req, res) => {
+  const messages = await getMessages()
+  res.render("messages", { title: "Mini Message Board", messages: messages, formatDate: formatDate });
+})
 
 module.exports = indexRouter;
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,13 +1,7 @@
 const { Router } = require("express")
-const { getMessages } = require('../controllers/messagesController')
-const { formatDate } = require("../utilities/dateFormatter")
 
 const newRouter = Router()
 
 newRouter.get("/", (req, res) => res.send("This is where you can send messages"))
-newRouter.get("/messages", async (req, res) => {
-    const messages = await getMessages()
-    res.render("messages", { messages: messages, formatDate: formatDate });
-})
 
 module.exports = newRouter

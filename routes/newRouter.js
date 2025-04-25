474b495e9ac0f16b74595e0cbbe1fe0ae35dd60c
emesefedev <emesefedev@gmail.com>
2025-04-25 12:40:40 +0200

    feat: new message form

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -2,6 +2,8 @@ const { Router } = require("express")
 
 const newRouter = Router()
 
-newRouter.get("/", (req, res) => res.send("This is where you can send messages"))
+newRouter.get("/", async (req, res) => {
+  res.render("form", { title: "New Message"});
+})
 
 module.exports = newRouter
